d8aa8e78b6bbde1506b3df0b14443222f57d50ca
thaitruong06 <24521599@gm.uit.edu.vn>
2026-05-17 15:05:11 +0700

    add Nhom11_NT101.Q21/Utils/Helper.cs

---
+++ Nhom11_NT101.Q21/Utils/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nhom11_NT101.Q21.Utils
+{
+    internal class Helper
+    {
+        public static void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
